e2d723d75b402becd602f5f8ce58abc5e8e57224
kachiejim <ejimovc@gmail.com>
2025-10-10 14:14:49 +0100

    updated the metadata files

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Always use root path for universal access
+  base: '/movic-tech/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
